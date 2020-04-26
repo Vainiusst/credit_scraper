f420e69f792a0d1fe55b3da70cc5986a62828928
Vainiusst <59452212+Vainiusst@users.noreply.github.com>
2020-04-26 19:19:51 +0300

    Add files via upload
    Addition to the current scraper - two more websites covered

--- moment.py
+++ moment.py
@@ -9,24 +9,104 @@ starting_time = time()
 
 chrome = webdriver.Chrome()
 
-amounts = [100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
-terms = [3, 6, 12, 24, 36, 48, 60, 72]
+# 7.332,
+
+amounts = [0, 58.656, 74.448, 141, 100.956, 19.74, 20.304, 28.2, 28.2, 28.2, 28.2, 28.2]
+terms = [3, 6, 12, 24, 36, 48, 60]
 
 chrome.get("https://www.momentcredit.lt")
 sleep(2)
 
 def set_amount(amount):
-    '''
-    100 = 17,5% sliderio judesio
-    '''
+    #full slider length - 564px
     slider = chrome.find_element_by_xpath('//*[@id="amount_slider_wrap"]/div/span/div[3]')
     move = ActionChains(chrome)
     move.drag_and_drop_by_offset(slider, amount, 0).perform()
 
-def terms_selector(term):
+def set_term(term):
     selector = Select(chrome.find_element_by_xpath('//*[@id="duration"]'))
     selector.select_by_value(str(term))
     sleep(2)
 
-terms_selector(3)
-set_amount(50)
+def read_installment():
+    std_ex = chrome.find_element_by_xpath('//*[@id="credit_rates_footnote"]/p[2]').get_attribute("innerHTML")
+    iso_str = re.search('mėnesinė įmoka būtų \d+.*\d*,+\d+ EUR', std_ex).group()
+    installment = re.search('\d+.*\d*,+\d+', iso_str).group().replace(".", "")
+    print(installment)
+    return installment
+
+def read_interest():
+    std_ex = chrome.find_element_by_xpath('//*[@id="credit_rates_footnote"]/p[2]').get_attribute("innerHTML")
+    iso_str = re.search('palūkanos būtų \d+,*\d* %', std_ex).group()
+    interest = re.search('\d+,*\d*', iso_str).group()
+    print(interest)
+    return interest
+
+def read_APR():
+    std_ex = chrome.find_element_by_xpath('//*[@id="credit_rates_footnote"]/p[2]').get_attribute("innerHTML")
+    iso_str = re.search('BVKKMN . \d+,\d+%', std_ex).group()
+    APR = re.search('\d+,\d+', iso_str).group()
+    print(APR)
+    return APR
+
+def read_admin():
+    std_ex = chrome.find_element_by_xpath('//*[@id="credit_rates_footnote"]/p[2]').get_attribute("innerHTML")
+    iso_str = re.search('ir \d,*\d* Eur/mėn\.', std_ex).group()
+    admin = re.search('\d,*\d*', iso_str).group()
+    print(admin)
+    return admin
+
+def suma():
+    inner = chrome.find_element_by_xpath('//*[@id="amount_slider_wrap"]/div/span/div[3]/span/span[2]').get_attribute("innerHTML")
+    suma = re.search('\d+.\d+ <', inner).group()[:-2]
+    if ' ' in suma:
+        return suma.replace(' ', '')
+    else:
+        return suma
+
+def write_content(term):
+    with open(r'.\moment_content.csv', newline='', mode='a', encoding='UTF-8') as db2:
+        sum = suma()
+        combo = f'{sum}/{term}'
+        installment = read_installment()
+        interest = read_interest()
+        APR = read_APR()
+        admin = read_admin()
+        csv_writer = csv.writer(db2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([combo, installment, interest, APR, admin])
+        sleep(2)
+
+
+def do_erryfin(amounts, terms):
+    for term in terms:
+        set_term(term)
+        if term == 3:
+            set_amount(-274.668)
+            for amount in amounts:
+                set_amount(amount)
+                write_content(term)
+        elif term in [6, 12, 24]:
+            set_amount(-556.668)
+            for amount in amounts:
+                set_amount(amount)
+                write_content(term)
+        elif term == 36:
+            set_amount(-423)
+            write_content(term)
+            for amount in amounts[3:]:
+                set_amount(amount)
+                write_content(term)
+        else:
+            set_amount(-282)
+            write_content(term)
+            for amount in amounts[4:]:
+                set_amount(amount)
+                write_content(term)
+        sleep(1)
+    chrome.close()
+
+
+do_erryfin(amounts, terms)
+ending_time = time()
+total_time = str((ending_time - starting_time)/60)
+print(f"Time to crunch through Moment Credit's data is {total_time} minutes.")
--- smspinigai.py
+++ smspinigai.py
@@ -1,40 +1,117 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep, time
-# import csv
+import csv
 import re
 
-# starting_time = time()
+starting_time = time()
 
 chrome = webdriver.Chrome()
 
-amounts = [500, 1000, 2000, 3000, 4000, 5000]
+#offsets (pixels), that set the slider at the correct position for the necessary amount/term
+
+amounts= [-543, 108.6, 108.6, 108.6, 108.6, 54.3, 54.3]
+
+terms = [-155.14, 77.59, 77.59, 77.59, 77.59, 77.59, 77.59, 77.59]
 
 def start_up():
     chrome.get("https://www.smspinigai.lt")
     nuolatinis = chrome.find_element_by_xpath('/html/body/div[1]/div[1]/main/section/div/div[2]/div/ul/li[2]')
     nuolatinis.click()
-    slider = chrome.find_element_by_xpath('//*[@id="slider_summa"]/div[2]/div[2]/div')
-    move = ActionChains(chrome)
-    move.click_and_hold(slider).move_by_offset(-204, 0).release().perform()
-    sleep(0.5)
-    slider = chrome.find_element_by_xpath('//*[@id="slider_period"]/div[2]/div[2]/div/div[1]')
-    move = ActionChains(chrome)
-    move.click_and_hold(slider).move_by_offset(-204, 0).release().perform()
-    sleep(0.5)
 
 def set_amount(amount):
-    plus_button = chrome.find_element_by_xpath('/html/body/div[1]/div[1]/main/section/div/div[1]/div[1]/span[2]')
-    int_amount = chrome.find_element_by_xpath('//*[@id="slider_summa"]/div[2]/div[2]/div/div[2]/span/span').get_attribute("innerHTML")[:-2]
-    amount_shown = re.search('\d+', int_amount).group()
-    while int(amount_shown) < int(amount):
-        plus_button.click()
-        print(amount_shown)
-        sleep(5)
-    print(int_amount)
-    sleep(2)
+    slider = chrome.find_element_by_xpath('//*[@id="slider_summa"]/div[2]/div[2]/div/div[1]')
+    move_amt = ActionChains(chrome)
+    move_amt.click_and_hold(slider).move_by_offset(amount, 0).release().perform()
 
-start_up()
-for amount in amounts:
-    set_amount(amount)
-chrome.close()
+def set_term(term):
+    slider = chrome.find_element_by_xpath('//*[@id="slider_period"]/div[2]/div[2]/div/div[1]')
+    move_term = ActionChains(chrome)
+    move_term.click_and_hold(slider).move_by_offset(term, 0).release().perform()
+
+def open_dets():
+    dets = chrome.find_element_by_xpath('/html/body/div[1]/div[1]/main/section/div/div[2]/div/div[4]/a')
+    dets.click()
+
+def kill_cookies():
+    cookies = chrome.find_element_by_xpath('//*[@id="cookie-vue-app"]/div/div/div[2]')
+    cookies.click()
+
+def read_installment():
+    installment = chrome.find_element_by_xpath('//*[@id="graph_modal"]/ul/li[3]').get_attribute("innerHTML")[:-2]
+    inst_str = re.search('\d+,\d+', installment).group()
+    print(inst_str)
+    return inst_str
+
+def read_interest():
+    interest = chrome.find_element_by_xpath('//*[@id="graph_modal"]/ul/li[5]').get_attribute("innerHTML")[:-2]
+    int_str = re.search('\d+,\d+', interest).group()
+    print(int_str)
+    return int_str
+
+def read_APR():
+    APR = chrome.find_element_by_xpath('//*[@id="graph_modal"]/ul/li[6]').get_attribute("innerHTML")[:-2]
+    APR_str = re.search('\d+,\d+', APR).group()
+    print(APR_str)
+    return APR_str
+
+def read_admin():
+    admin = chrome.find_element_by_xpath('//*[@id="graph_modal"]/ul/li[4]').get_attribute("innerHTML")[:-2]
+    admin_str = re.search('\d+,\d+', admin).group()
+    return admin_str
+
+def turn_to_proc(func):
+    number = func.replace(",", ".")
+    number_fl = float(number)
+    amount = chrome.find_element_by_xpath('//*[@id="graph_modal"]/ul/li[1]').get_attribute("innerHTML")[:-2]
+    amount_fl = float(re.search('\d+,\d+', amount).group().replace(",", "."))
+    proc = str((number_fl*100)/amount_fl)
+    proc_str = proc.replace(".", ",")
+    print(proc_str)
+    return proc_str
+
+def to_mths(string):
+    if string[2:] == " mėn.":
+        return int(string[1])
+    else:
+        no_yrs = int(string[0])
+        return no_yrs*12
+
+def write_content():
+    with open(r'.\smspinigai_content.csv', newline='', mode='a', encoding='UTF-8') as db2:
+        suma = chrome.find_element_by_xpath('//*[@id="slider_summa"]/div[2]/div[2]/div/div[2]/span/span').get_attribute("innerHTML")[:-2]
+        terminas_pre = chrome.find_element_by_xpath('//*[@id="slider_period"]/div[2]/div[2]/div/div[2]/span/span').get_attribute("innerHTML")
+        terminas = to_mths(terminas_pre)
+        combo = f'{suma}/{terminas}'
+        installment = read_installment()
+        interest = read_interest()
+        APR = read_APR()
+        admin = turn_to_proc(read_admin())
+        csv_writer = csv.writer(db2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([combo, installment, interest, APR, admin])
+
+def close_dets():
+    close_bttn = chrome.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div[2]/span')
+    close_bttn.click()
+
+def do_erryfin(amounts, terms):
+    start_up()
+    kill_cookies()
+    sleep(1)
+    for term in terms:
+        set_term(term)
+        for amount in amounts:
+            set_amount(amount)
+            sleep(1)
+            open_dets()
+            sleep(1)
+            write_content()
+            close_dets()
+            sleep(1)
+    chrome.close()
+
+
+do_erryfin(amounts, terms)
+ending_time = time()
+total_time = str((ending_time - starting_time)/60)
+print(f"Time to crunch through Smspinigai data is {total_time} minutes.")
