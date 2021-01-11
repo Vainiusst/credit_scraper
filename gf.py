c66f35769523d343ff466fe6b87504241c58be49
Vainiusst <vainius1994@gmail.com>
2021-01-11 11:34:03 +0200

    Logic changes implemented

--- gf.py
+++ gf.py
@@ -12,7 +12,7 @@ class GF:
 
     def __init__(self):
         self.chrome = webdriver.Chrome()
-        self.amounts = [100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000]
+        self.amounts = [100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 12000, 15000, 20000, 25000]
         self.terms = [3, 6, 12, 24, 36, 48, 60, 72, 84]
 
     def waiter(self):
@@ -69,6 +69,14 @@ class GF:
         admin_form = str(round(float(admin_str) / 100, 4)).replace(".", ",")
         return admin_form
 
+    def read_contract(self, term):
+        """Reads the contract fee and returns it properly formatted to be written into CSV"""
+        contract = self.chrome.find_element_by_css_selector('#text_SN_SERVICEFEE').get_attribute("innerHTML")
+        contract_str = re.search('\d+,\d+', contract).group().replace(",", ".")
+        contract_corr = float(contract_str)/term
+        contract_form = str(round(contract_corr, 4)).replace(".", ",")
+        return contract_form
+
     def write_content(self, amount, term):
         """Writes the content into the CSV file"""
         with open('./gf_content.csv', newline='', mode='a', encoding='UTF-8') as db2:
@@ -77,8 +85,9 @@ class GF:
             interest = self.read_interest()
             APR = self.read_APR()
             admin = self.read_admin()
+            contract = self.read_contract(term)
             csv_writer = csv.writer(db2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            csv_writer.writerow([combo, installment, interest, APR, admin])
+            csv_writer.writerow([combo, installment, interest, APR, admin, contract])
 
     def do_erryfin(self, amounts, terms):
         """Combines all the actions necessary for the program to work into a single function"""
@@ -89,9 +98,15 @@ class GF:
         for term in self.terms:
             self.set_term(term)
             for amount in self.amounts:
-                if amount == 100 and term >= 72:
+                if amount == 100 and term >= 48:
                     pass
-                    # This company does not issue 100€ for terms longer than 72 months, hence these combos are escaped
+                    # This company does not issue 100€ for terms longer than 48 months, hence these combos are escaped
+                elif amount < 3000 and term >= 60:
+                    pass
+                elif amount < 5000 and term >= 72:
+                    pass
+                elif amount < 8000 and term >= 84:
+                    pass
                 else:
                     self.set_amount(amount)
                     self.waiter()
